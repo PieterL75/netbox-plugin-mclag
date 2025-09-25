7bf7014657b7c5457a12132e28bcc451bc40bae8
Pieter Lambrecht <pieter.lambrecht@accenture.com>
2025-09-25 09:44:30 +0200

    Update version and Author

--- netbox_plugin_mclag/__init__.py
+++ netbox_plugin_mclag/__init__.py
@@ -5,8 +5,11 @@ class NetBoxMcLagConfig(PluginConfig):
     name = "netbox_plugin_mclag"
     verbose_name = "Multi-Chassis LAG"
     description = "Manage Multi-Chassis Link Aggregation Groups in Netbox (MC-LAG / MLAG / vPC / etc)"
-    version = "0.2.2"
+    author = "Pieter Lambrecht"
+    author_email = "pieter.lambrecht@gmail.com"
+    version = "0.3.0"
     base_url = "mclag"
 
+# based original code of pv2b: https://github.com/pv2b/netbox-plugin-mclag
 
 config = NetBoxMcLagConfig
